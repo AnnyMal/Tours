c5accb954976554ba1ae5912bbc6180326d8a51e
AnnyMal <89228060160q@gmail.com>
2023-05-19 18:40:30 +0300

    добавлен InjectModel в order.service.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { UsersService } from './services/users/users.service';
 import {UsersModule} from "./controller/users/users.module";
 import {MongooseModule} from "@nestjs/mongoose";
 import {ToursModule} from "./controller/tours/tours.module";
+import {OrderModule} from "./controller/order/order.module";
 
 @Module({
-  imports: [UsersModule, ToursModule,
+  imports: [UsersModule, ToursModule, OrderModule,
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/nest')],
   controllers: [AppController],
   providers: [AppService],
